745aa52d4ee8db2fec07f804e54be28c9b9078fe
Sampo Siltanen <sampo.siltanen@reaktor.com>
2022-12-04 11:55:31 +0200

    Golfed day 4

--- fsharp/day04.fsx
+++ fsharp/day04.fsx
@@ -1,18 +1,9 @@
-let assignmentPairs = 
-  System.IO.File.ReadAllLines "data/day04.txt"
-  |> Array.map (fun row -> row.Split('-', ',') |> Array.map int)
-
-let completeOverlap [| aStart; aEnd; bStart; bEnd |] = 
-  (aStart <= bStart && aEnd >= bEnd) || (aStart >= bStart && aEnd <= bEnd)
-
-let partialOverlap [| aStart; aEnd; bStart; bEnd |] = 
-  (aEnd >= bStart && aStart <= bEnd) || (bEnd >= aStart && bStart <= aEnd)
-
 let (task1, task2) = 
-  assignmentPairs
-  |> Array.fold (fun (task1, task2) pairs -> 
-    task1 + System.Convert.ToInt32(completeOverlap(pairs)),
-    task2 + System.Convert.ToInt32(partialOverlap(pairs))) 
+  System.IO.File.ReadAllLines "data/day04.txt"
+  |> Array.fold (fun (task1, task2) row -> 
+    let [| a1; a2; b1; b2 |] = row.Split('-', ',') |> Array.map int
+    task1 + System.Convert.ToInt32((a1 <= b1 && a2 >= b2) || (a1 >= b1 && a2 <= b2)),
+    task2 + System.Convert.ToInt32((a2 >= b1 && a1 <= b2) || (b2 >= a1 && b1 <= a2))) 
     (0,0)
 
 printfn "Day 4 Task 1: %i" task1
